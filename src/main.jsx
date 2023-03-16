83ed89579714cf26d2aad09df1b10d3ed73ba804
Shivam Yadav <shivamyadav93000@gmail.com>
2023-03-16 22:29:11 +0530

    okk

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import InputForm from './form'
+import FormValidation from './form-validetion'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InputForm/>
+    {/* <InputForm/> */}
+    <FormValidation/>
   </React.StrictMode>,
 )
