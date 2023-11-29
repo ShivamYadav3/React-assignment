9a66630b3e05edd6583dbb2771ee26f04dbc99dd
Shivam Yadav <shivamyadav93000@gmail.com>
2023-11-30 00:51:58 +0530

    done

--- src/IP/OTP/otp.jsx
+++ src/IP/OTP/otp.jsx
@@ -1,16 +1,33 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 
 const Otp = () => {
   const [inputField, setInputField] = useState(4);
+  const [separator, setSeparator] = useState("-");
+  const [value, setValue] = useState(Array(inputField).fill(""));
+  const [type, setType] = useState("text");
+  const [placeholder, setPlaceholder] = useState(Array(inputField).fill(""));
   const arr = Array.from({ length: inputField });
-  const inputRefs = arr.map(() => useRef(null));
+
   const ChangeInput = (e, i) => {
+    const updatedValue = [...value];
+    updatedValue[i] = e.target.value;
+    setValue(updatedValue);
+
     if (e.target.value && i < arr.length - 1) {
-      inputRefs[i + 1].current.focus();
+      document.getElementById(`input-${i + 1}`).focus();
     } else if (!e.target.value && i > 0) {
-      inputRefs[i - 1].current.focus();
+      document.getElementById(`input-${i - 1}`).focus();
     }
   };
+
+  const clearOtp = () => {
+    setValue(Array(inputField).fill(""));
+  };
+
+  const handleGetOtp = () => {
+    alert(`OTP Text: ${value.join("")}`);
+  };
+
   return (
     <div className="flex mx-8 mt-7">
       <div className="flex flex-col gap-8">
@@ -31,55 +48,90 @@ const Otp = () => {
             <div>
               <label htmlFor="">separator</label>
             </div>
-            <input type="text" className="border px-5 outline-none h-6 py-2 " />
+            <input
+              type="text"
+              className="border px-5 outline-none h-6 py-2 "
+              value={separator}
+              onChange={(e) => setSeparator(e.target.value)}
+            />
           </div>
           <div>
             <div>
               <label htmlFor="">Value</label>
             </div>
-            <input type="text" className="border px-5 outline-none h-6 py-2" />
+            <input
+              type="text"
+              className="border px-5 outline-none h-6 py-2"
+              value={value.join("")}
+              onChange={(e) => setValue(Array.from(e.target.value))}
+            />
           </div>
           <div>
             <div>
               <label htmlFor="">Placeholder</label>
             </div>
-            <input type="text" className="border px-5 outline-none h-6 py-2" />
+            <input
+              type="text"
+              className="border px-5 outline-none h-6 py-2"
+              value={placeholder.join("")}
+              onChange={(e) => setPlaceholder(Array.from(e.target.value))}
+            />
           </div>
           <div>
             <div>
-              <label htmlFor="">Input text</label>
+              <label htmlFor="">Input Type</label>
             </div>
-            <input type="text" className="border px-5 outline-none h-6 py-2" />
+            <select
+              name="type"
+              id="type"
+              className="border px-5 outline-none h-6 py-2 text-black"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value="text">text</option>
+              <option value="number">number</option>
+              <option value="password">Password</option>
+              <option value="tel">tel</option>
+            </select>
           </div>
         </div>
       </div>
       <div className="flex justify-center item-center w-[100vw] h-[100vh]">
         <div className="flex flex-col gap-10">
           <div className="text-3xl font-bold">Enter Verification code</div>
-          <div className="flex gap-8">
-            {arr.map((data, i) => {
-              return (
-                <div key={i}>
+          <div className="flex gap-4">
+            {arr.map((data, i) => (
+              <div key={i} className="flex gap-4">
+                <div>
                   <input
-                    type="text"
-                    ref={inputRefs[i]}
+                    type={type}
+                    id={`input-${i}`}
                     className="border-2 w-8 h-8 rounded-md text-center outline-none"
+                    value={value[i]}
                     maxLength={1}
-                    autoFocus={i == 0}
+                    placeholder={placeholder[i]}
+                    autoFocus={i === 0}
                     onChange={(e) => ChangeInput(e, i)}
                   />
                 </div>
-              );
-            })}
+                {i < arr.length - 1 && <div>{separator}</div>}
+              </div>
+            ))}
           </div>
           <div className="flex gap-8">
             <div>
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                onClick={clearOtp}
+              >
                 clear
               </button>
             </div>
             <div>
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                onClick={handleGetOtp}
+              >
                 get otp
               </button>
             </div>
